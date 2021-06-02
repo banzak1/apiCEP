fb4aceffcd65d9d966053cd68ee9b85a962852ea
Banzak-Uninter <leosantanabr@gmail.com>
2021-06-02 19:01:45 -0300

    renderização das informações

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var submitButton = document.querySelector('#app form button')
 var zipCodeField = document.querySelector('#app form input')
-var content = document.querySelector('#app form main')
+var content = document.querySelector('#app main')
 
 submitButton.addEventListener('click', run)
 
@@ -13,11 +13,21 @@ function run(event) {
     zipCode = zipCode.replace('.', '')
     zipCode = zipCode.trim()
 
-    axios.get('https://viacep.com.br/ws/' + zipCode + '/json/')
+    axios
+    .get('https://viacep.com.br/ws/' + zipCode + '/json/')
     .then(function (response) {
-
+        console.log(response.data)
+        createLine(response.data.localidade)
     })
     .catch(function (error) {
-        
+        console.log(error)
     })
+}
+
+function createLine(text) {
+    var line = document.createElement('p')
+    var text = document.createTextNode(text)
+
+    line.appendChild(text)
+    content.appendChild(line)
 }
